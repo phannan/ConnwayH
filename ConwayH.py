25ab0a2a8dd637c0313943caebc56cb4580169fa
Paula Hannan <paula@tovabi.com>
2017-10-20 14:22:44 -0700

    extra files

---
+++ ConwayH.py
@@ -0,0 +1,4 @@
+## Overpopulation: if a living cell is surrounded by more than three living cells, it dies.
+## Stasis: if a living cell is surrounded by two or three living cells, it survives.
+## Underpopulation: if a living cell is surrounded by fewer than two living cells, it dies.
+##  Reproduction: if a dead cell is surrounded by exactly three cells, it becomes a live cell.
