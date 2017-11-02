cc76cbc1ad76d083021178c7ca578ffc86098c11
Paula Hannan <paula@tovabi.com>
2017-11-02 12:14:25 -0700

    add test file and .gitignore

--- ConwayH.py
+++ ConwayH.py
@@ -2,3 +2,7 @@
 ## Stasis: if a living cell is surrounded by two or three living cells, it survives.
 ## Underpopulation: if a living cell is surrounded by fewer than two living cells, it dies.
 ##  Reproduction: if a dead cell is surrounded by exactly three cells, it becomes a live cell.
+class Life:
+    def __init__(self):
+        # type: () -> object
+        pass
---
+++ test_life.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest import TestCase
+import sys
+
+#class TestLife(unittest.TestCase):
+#    failIf = (1 == 1)
+
+#print(sys.version)
+
+class test_Life2(TestCase):
+    def test_red(self):
+        assert(1 == 11)
+      #  self.fail('red')
+
+
+#unittest.main()
