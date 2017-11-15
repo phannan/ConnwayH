b8b66a10c1fdc846ab946a846742c0402e4a0571
Paula Hannan <paula@tovabi.com>
2017-11-15 10:00:23 -0800

    create base

--- test_life.py
+++ test_life.py
@@ -2,11 +2,22 @@ import unittest
 from Conway.Conway import *
 
 
-class test_Life(unittest.TestCase):
 
-      def test_life_smoke(self):
-        life = Life
-        self.assertEqual(1,1)
+
+## Overpopulation: if a living cell is surrounded by more than three living cells, it dies.
+##
+##   ***
+##   **.
+##
+##  Goes to
+##
+##   ***
+##   *..
+##
+## Stasis: if a living cell is surrounded by two or three living cells, it survives.
+## Underpopulation: if a living cell is surrounded by fewer than two living cells, it dies.
+## Reproduction: if a dead cell is surrounded by exactly three cells, it becomes a live cell.
+
 
 
 if __name__ == '__main__':
