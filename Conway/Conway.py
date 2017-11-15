7c672ab22e17111dde300297aaf932cffdd7d035
Paula Hannan <paula@tovabi.com>
2017-11-15 15:08:38 -0800

    trying to birth some cells

--- Conway/Conway.py
+++ Conway/Conway.py
@@ -1,21 +1,62 @@
 ## Overpopulation: if a living cell is surrounded by more than three living cells, it dies.
-## Stasis: if a living cell is surrounded by two or three living cells, it survives.
 ## Underpopulation: if a living cell is surrounded by fewer than two living cells, it dies.
-##  Reproduction: if a dead cell is surrounded by exactly three cells, it becomes a live cell.
+
+## Stasis: if a living cell is surrounded by two or three living cells, it survives.
+## Reproduction: if a dead cell is surrounded by exactly three cells, it becomes a live cell.
 
 # everything should assume to die unless it has a reason to live.
-class Life:
-    # def __init__(starting_point : set):
-    def __init__(self,starting_point : set):
-        # type: () -> object
-        pass
 
-    def next(starting_point : set):
-    # everything should assume to die unless it has a reason to live.
-        empty_board = set()
-        return (empty_board)
+
+
+class Life(object):
+    # def __init__(starting_point : set):
+    def __init__(self, starting_point : set):
+        self.alive_cells = starting_point
+
+
+    def next(self):
+        # everything should assume to die unless it has
+        #   a reason to live.
+        # board = set()
+        board = set()
+
+        for (x, y) in self.alive_cells:
+            cnt = count_live_neighbors((x, y))
+            # print(x, ' ', y, ' cnt = ', cnt)
+            if (cnt == 2 or cnt == 3):
+                board.add((x, y))
+
+        return (board)
+
+
+
+        ## Reproduction: if a dead cell is surrounded
+        ##  by exactly three cells, it lives!
+
+        return (board)
 
     # return set()
+
+        ## Stasis: if a living cell is surrounded by
+        #  two or three living cells, it survives.
+
+
+
+def count_dead_in_board(board:set):
+    if board.__len__() == 0 :
+        return 0
+    else :
+        max_x = 0
+        max_y = 0
+        for (x,y) in board:
+            if x > max_x : max_x = x
+            if y > max_y : max_y = y
+
+        living = board.__len__()
+        total_board = (max_x+1)*(max_y+1)
+        # print('living= ',living, ' ', max_x,' ',max_y)
+
+        return ( total_board - living)
 
 def count_live_neighbors(board : set, cell : tuple):
     start = 0
--- test_life.py
+++ test_life.py
@@ -5,6 +5,10 @@ from Conway.Conway import *
 # ..
 b_corner      = set([(0, 0)])
 
+# *.*
+# .*.
+b_triangle = set([(0,0),(0,2),(1,1)])
+
 # this one always stays the same
 # **
 # **
@@ -14,6 +18,12 @@ b_stable      = set([(0, 0), (0, 1), (1, 0), (1, 1)])
 ##   **.
 #
 b_3top_2below = set([(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)])
+
+#   ...
+#   ***
+#   ...
+#
+b_blinker = set([(0,1),(1,1),(2,1)])
 
 class conway_test(unittest.TestCase):
 #  Helps me make sure tests are running.
@@ -29,7 +39,12 @@ class conway_test(unittest.TestCase):
         board = set([(0, 0)])
         self.assertEqual(count_live_neighbors(board, (0, 1)), 1)
 
-    def test_how_many_live_neighbors_simple(self):
+    def test_how_many_live_neighbors_stable(self):
+           for (x,y) in b_stable :
+              self.assertEqual(
+                  count_live_neighbors(b_stable, (x,y)), 3)
+
+    def test_how_many_live_neighbors_b3top(self):
         ##   ***
         ##   **.
         #
@@ -45,9 +60,10 @@ class conway_test(unittest.TestCase):
     ##
 
     def test_stable_stays_same(self):
-        self.assertEqual(Life.next(b_stable),b_stable)
+        # self.assertEqual(Life.next(b_stable),b_stable)
+        self.assertEqual(b_stable,Life.next(b_stable))
 
-    def hid_test_only_one_dies(self):
+    def test_only_one_dies(self):
         self.assertEqual(Life.next(b_corner),set())
     ##
     ##   ***
@@ -55,14 +71,50 @@ class conway_test(unittest.TestCase):
     ##
     ##  Goes to
     ##
-    ##   ***
+    ##   *.*
     ##   *..
     ##
-    def hid_test_board_is_overpopulated(self):
+    def test_board_is_overpopulated(self):
 
-        next_gen = set([(0, 0), (0, 1), (0, 2), (1, 0)])
-        game = Life(b_3top_2below)
+        next_gen = set([(0, 0), (0, 2), (1, 0)])
+        game = Life.next(b_3top_2below)
         self.assertEqual(game, next_gen)
+
+    def test_count_dead_cells(self):
+
+        self.assertEqual(
+            count_dead_in_board(b_corner),  0)
+        self.assertEqual(
+            count_dead_in_board(b_triangle),3)
+        self.assertEqual(
+            count_dead_in_board(b_stable),0)
+        self.assertEqual(
+            count_dead_in_board(b_3top_2below), 1)
+        self.assertEqual(
+            count_dead_in_board(b_blinker), 3)
+
+
+    # *.*
+    # *..
+    #  goes to
+    # .*.
+    # ...
+    def hid_test_birth(self):
+        next_gen = set([(0,1)])
+
+        game = Life.next(b_triangle)
+        self.assertEqual(game,next_gen)
+#   ...
+#   ***
+#   ...
+#  goes to
+#   .*.
+#   .*.
+#   .*.
+    def hid_test_blinker(self):
+        next_gen = set([(1,0),(1,1),(1,2)])
+        game = Life.next(b_blinker)
+        self.assertEqual(game,next_gen)
 
 
 
