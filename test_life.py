d6672a728fb74e9c613a377e9d08aeec51991641
Paula Hannan <paula@tovabi.com>
2017-11-18 18:09:48 -0800

    Cleaner Starting Point after code retreat and before delete of file

--- test_life.py
+++ test_life.py
@@ -6,29 +6,6 @@ from Conway.Conway import *
 ## Stasis: if a living cell is surrounded by two or three living cells, it survives.
 ## Reproduction: if a dead cell is surrounded by exactly three cells, it becomes a live cell.
 
-# # *.
-# # ..
-# b_corner      = set([(0, 0)])
-#
-# # *.*
-# # .*.
-# b_triangle = set([(0,0),(0,2),(1,1)])
-#
-# # this one always stays the same
-# # **
-# # **
-# b_stable      = set([(0, 0), (0, 1), (1, 0), (1, 1)])
-#
-# ##   ***
-# ##   **.
-# #
-# b_3top_2below = set([(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)])
-#
-# #   ...
-# #   ***
-# #   ...
-# #
-# b_blinker = set([(0,1),(1,1),(2,1)])
 
 class conway_test(unittest.TestCase):
 #  Helps me make sure tests are running.
